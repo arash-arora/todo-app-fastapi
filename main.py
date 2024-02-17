802e5cb33a0aebe6829a9ca344ee51e807204618
Arash Arora <arasharora2001@gmail.com>
2024-02-17 21:04:33 +0530

    todo app using fastapi

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+import models
+from database import engine
+from routers import auth, todos, users
+from fastapi.staticfiles import StaticFiles
+from fastapi import status
+from fastapi.responses import RedirectResponse
+
+app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/")
+async def root():
+    return RedirectResponse(url="/todos", status_code=status.HTTP_302_FOUND)
+
+app.include_router(auth.router)
+app.include_router(todos.router)
+app.include_router(users.router)
